f2bddf8a48b9dccd45c54c8f0a8a1df09632b394
Markus Mueller <147660115+RetryR1v2@users.noreply.github.com>
2024-10-13 15:19:16 +0200

    Delete installation directory

--- installation/mms_lotteryjackpot.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `mms_lotteryjackpot` (
-	`jackpot` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8_general_ci',
-	`money` INT(11) NULL DEFAULT NULL
-)
-COLLATE='utf8_general_ci'
-ENGINE=InnoDB
-;
